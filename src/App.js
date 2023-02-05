0aa578af3a0215b4dfc36f29eb1fcf8ee08edad9
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-02-05 21:25:40 +0530

    Updated Note with Props

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Note from './components/Note';
+import notes from './notes'
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
       <Header />
-      <Note />
+      {notes.map((note) => (
+          <Note
+            key={note.key}
+            title={note.title}
+            content={note.content}
+          />
+      ))}
       <Footer />
     </div>
+  
   );
 }
 
