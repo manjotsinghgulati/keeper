247a4284d9655b8f9cbfb1fe2f135564ea6873c7
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-02-12 01:04:09 +0530

    Note Creation & Deletion

--- src/App.js
+++ src/App.js
@@ -3,19 +3,44 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Note from './components/Note';
 import notes from './notes'
+import CreateArea from './components/CreateArea';
+import { useState } from 'react';
 
 
 const App = () => {
+
+const [notes, setNotes]  = useState([]);
+
+  const addNote = (note) => {
+    setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  }
+
+  const onDelete = (id) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div className="App">
       <Header />
-      {notes.map((note) => (
+      <CreateArea
+        onAdd={addNote}
+      />
+      {notes.map((note, index) => (
           <Note
-            key={note.key}
+            id={index}
+            key={index}
             title={note.title}
             content={note.content}
+            onDelete={onDelete}
           />
       ))}
+      
       <Footer />
     </div>
   
